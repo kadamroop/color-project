1e6d752c11517c63b3720cc304f3fd400c2c79a4
kadamroop <roopkadam123@gmail.com>
2021-05-25 23:56:05 +0530

    colour helpers added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import Palette from './Components/Palette/Palette';
 import SeedColours from './SeedColors';
+import { generatePalette } from './colorHelpers'
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div >
-      <Palette {...SeedColours[4]}/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    console.log('generatePalette', generatePalette(SeedColours[4]))
+    return (
+      <div >
+        <Palette {...SeedColours[4]} />
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div >
+//       <Palette {...SeedColours[4]}/>
+//     </div>
+//   );
+// }
 
 export default App;
--- src/Components/ColorBox/ColorBox.js
+++ src/Components/ColorBox/ColorBox.js
@@ -1,13 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import './ColorBox.css'
 
 class ColorBox extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { copied: false };
+        this.changeCopyState = this.changeCopyState.bind(this)
+    }
+
+    changeCopyState() {
+        // this.setState({ copied: true })
+        this.setState({ copied: true }, () => {
+            setTimeout(() => this.setState({ copied: false }), 1500);
+        });
+    }
     render() {
         const { name, background } = this.props
+        const { copied } = this.state;
+        console.log('copied', copied)
         return (
-            <CopyToClipboard text={background}>
+            <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div style={{ background }} className="ColorBox">
+                    <div style={{ background }}
+                        className={`copy-overlay ${copied && 'show'}`}
+                    />
+                    <div className={`copy-msg  ${copied && 'show'}`}>
+                        <h1>Copied!!!</h1>
+                        <p>{this.props.background}</p>
+                    </div>
                     <div className="copy-container">
                         <div className="box-content">
                             <span>{name}</span>
@@ -26,19 +47,33 @@ export default ColorBox;
 
 
 
-
 // function ColorBox(props) {
+//     const [copied, setCopied] = useState(false);
 //     const { name, background } = props
+
+//     const changeCopyState = () => {
+//         console.log('copied', copied)
+//         setCopied(true)
+//         setTimeout(() => {
+//             setCopied(false);
+//         }, 1500);
+//     }
+
 //     return (
-//         <div style={{ background }} className="ColorBox">
-//             <div className="copy-container">
-//                 <div className="box-content">
-//                     <span>{name}</span>
+//         <CopyToClipboard text={background} onCopy={changeCopyState}>
+//             <div style={{ background }} className="ColorBox">
+//                 <div style={{ background }}
+//                     className={`copy-overlay ${copied && "show"}`}
+//                 />
+//                 <div className="copy-container">
+//                     <div className="box-content">
+//                         <span>{name}</span>
+//                     </div>
+//                     <button className="copy-button">Copy</button>
 //                 </div>
-//                 <button className="copy-button">Copy</button>
+//                 <span className="see-more">More</span>
 //             </div>
-//             <span className="see-more">More</span>
-//         </div>
+//         </CopyToClipboard>
 //     )
 // }
 // export default ColorBox;
